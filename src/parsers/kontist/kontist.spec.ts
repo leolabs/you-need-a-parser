e1a22150a4f47dc89acfb5b491b173730f1c04c4
Leo Bernard <admin@leolabs.org>
2019-04-04 01:28:08 +0200

    :sparkles: Implement Kontist support

---
+++ src/parsers/kontist/kontist.spec.ts
@@ -0,0 +1,57 @@
+import { generateYnabDate, kontist } from './kontist';
+import { YnabRow } from '..';
+
+const content = `booking_date,valuta_date,amount,name,purpose,end_to_end_id,booking_status
+2018-07-19,2018-07-19,-18000,John Doe,Invoice 2018006,E-e5a5084f898385ba8bc869c9627d8d2,processed
+2018-07-19,2018-07-19,55800,Company,LB-5-2018.2018-07-15.CRIS.10001,NOTPROVIDED,processed`;
+
+const ynabResult: YnabRow[] = [
+  {
+    Date: '07/19/2018',
+    Payee: 'John Doe',
+    Memo: 'Invoice 2018006',
+    Outflow: '180.00',
+    Inflow: undefined,
+  },
+  {
+    Date: '07/19/2018',
+    Payee: 'Company',
+    Memo: 'LB-5-2018.2018-07-15.CRIS.10001',
+    Outflow: undefined,
+    Inflow: '558.00',
+  },
+];
+
+describe('Kontist Parser Module', () => {
+  describe('Matcher', () => {
+    it('should match Kontist files by fields', async () => {
+      const file = new File([content], 'test.csv');
+      const result = await kontist.match(file);
+      expect(result).toBe(true);
+    });
+
+    it('should not match empty files', async () => {
+      const file = new File([], 'test.csv');
+      const result = await kontist.match(file);
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('Parser', () => {
+    it('should parse data correctly', async () => {
+      const file = new File([content], 'test.csv');
+      const result = await kontist.parse(file);
+      expect(result).toEqual(ynabResult);
+    });
+  });
+
+  describe('Date Converter', () => {
+    it('should convert dates correctly', () => {
+      expect(generateYnabDate('2018-09-01')).toEqual('09/01/2018');
+    });
+
+    it('should throw an error when the input date is incorrect', () => {
+      expect(() => generateYnabDate('1.1.1')).toThrow('not a valid date');
+    });
+  });
+});
---
+++ src/parsers/kontist/kontist.ts
@@ -0,0 +1,72 @@
+import 'mdn-polyfills/String.prototype.startsWith';
+import { ParserFunction, MatcherFunction, ParserModule } from '..';
+import { parse } from '../../util/papaparse';
+
+export interface KontistRow {
+  booking_date: string;
+  valuta_date: string;
+  amount: string;
+  name: string;
+  purpose: string;
+  end_to_end_id: string;
+  booking_status: string;
+}
+
+export const generateYnabDate = (input: string) => {
+  const match = input.match(/(\d{4})-(\d{2})-(\d{2})/);
+
+  if (!match) {
+    throw new Error('The input is not a valid date. Expected format: YYYY-MM-DD');
+  }
+
+  const [, year, month, day] = match;
+  return [month.padStart(2, '0'), day.padStart(2, '0'), year].join('/');
+};
+
+export const kontistParser: ParserFunction = async (file: File) => {
+  const { data } = await parse(file, { header: true });
+
+  return (data as KontistRow[])
+    .filter(r => r.booking_date && r.amount)
+    .map(r => ({
+      Date: generateYnabDate(r.booking_date),
+      Payee: r.name,
+      Memo: r.purpose,
+      Outflow:
+        Number(r.amount) < 0 ? (-Number(r.amount) / 100).toFixed(2) : undefined,
+      Inflow: Number(r.amount) > 0 ? (Number(r.amount) / 100).toFixed(2) : undefined,
+    }));
+};
+
+export const kontistMatcher: MatcherFunction = async (file: File) => {
+  const requiredKeys: (keyof KontistRow)[] = [
+    'booking_date',
+    'valuta_date',
+    'name',
+    'purpose',
+    'end_to_end_id',
+    'booking_status',
+  ];
+
+  const { data } = await parse(file, { header: true });
+
+  if (data.length === 0) {
+    return false;
+  }
+
+  const keys = Object.keys(data[0]);
+  const missingKeys = requiredKeys.filter(k => !keys.includes(k));
+
+  if (missingKeys.length === 0) {
+    return true;
+  }
+
+  return false;
+};
+
+export const kontist: ParserModule = {
+  name: 'Kontist',
+  link: 'https://intercom.help/kontist/konto/konto-export-von-kontoauszugen',
+  match: kontistMatcher,
+  parse: kontistParser,
+};
