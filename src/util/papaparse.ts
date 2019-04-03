49bbeabcbb426cfd51c80d15ac410675b8cb8099
Leo Bernard <admin@leolabs.org>
2019-04-03 23:20:17 +0200

    :label: Add string type to parse function

--- src/util/papaparse.ts
+++ src/util/papaparse.ts
@@ -1,8 +1,11 @@
 import { parse as papaParse, ParseConfig, ParseResult } from 'papaparse';
 
-export const parse = (file: File, config?: ParseConfig): Promise<ParseResult> =>
+export const parse = (
+  file: File | string,
+  config?: ParseConfig,
+): Promise<ParseResult> =>
   new Promise((complete, error) => {
-    papaParse(file, {
+    papaParse(file as any, {
       ...config,
       complete,
       error,
