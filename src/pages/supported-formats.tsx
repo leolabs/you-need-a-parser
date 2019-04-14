0fe2b61ab8671a473e39e563111e96618d604c66
Leo Bernard <admin@leolabs.org>
2019-04-14 17:49:54 +0200

    :lipstick: Style supported formats better

--- src/pages/supported-formats.tsx
+++ src/pages/supported-formats.tsx
@@ -10,6 +10,17 @@ const Container = styled.div`
   margin: auto;
 `;
 
+const ParserPill = styled.a`
+  display: inline-block;
+  padding: 0.1rem 0.8rem;
+  border-radius: 100px;
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  text-decoration: none;
+  background: #3ebd93;
+  color: #fff !important;
+`;
+
 const SupportedFormats = () => (
   <>
     <MetaTags title="Supported Formats" />
@@ -18,19 +29,16 @@ const SupportedFormats = () => (
       <h1>You Need A Parser</h1>
       <h2>Supported Formats</h2>
 
-      {countries.map(c => (
+      {['international', ...countries].map(c => (
         <>
           <h3>{c}</h3>
           <p>
             {parsers
               .filter(p => p.country === c)
               .map(p => (
-                <>
-                  <a href={p.link} target="_blank">
-                    {p.name}
-                  </a>
-                  ,{' '}
-                </>
+                <ParserPill href={p.link} target="_blank">
+                  {p.name}
+                </ParserPill>
               ))}
           </p>
         </>
