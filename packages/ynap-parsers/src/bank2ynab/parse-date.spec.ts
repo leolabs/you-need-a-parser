9a81a7b09cf0be2c7a81507f30819ca1ee13f712
Leo Bernard <admin@leolabs.org>
2019-06-18 16:05:54 +0200

    :white_check_mark: Improve date parser test coverage

--- packages/ynap-parsers/src/bank2ynab/parse-date.spec.ts
+++ packages/ynap-parsers/src/bank2ynab/parse-date.spec.ts
@@ -1,4 +1,4 @@
-import { parseDate } from './parse-date';
+import { parseDate, ynabDate } from './parse-date';
 import { format } from 'date-fns';
 
 describe('bank2ynab Date Parser', () => {
@@ -14,5 +14,15 @@ describe('bank2ynab Date Parser', () => {
 
     const result4 = parseDate('10 Feb 18', '%d %b %y');
     expect(format(result4, 'MM/dd/yyyy')).toBe('02/10/2018');
+
+    const result5 = parseDate('2019-03-04');
+    expect(format(result5, 'MM/dd/yyyy')).toBe('03/04/2019');
+  });
+});
+
+describe('bank2ynab YNAB Date Formatter', () => {
+  it('should format dates correctly according to the YNAB format', () => {
+    const result1 = ynabDate(new Date('2018-03-18'));
+    expect(result1).toBe('03/18/2018');
   });
 });
