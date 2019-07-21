e8aaae171d6ab00e320d30571e24df3619107692
Leo Bernard <admin@leolabs.org>
2019-07-21 19:52:52 +0200

    :loud_sound: Log matches when there is more than one in tests

--- packages/ynap-parsers/src/index.spec.ts
+++ packages/ynap-parsers/src/index.spec.ts
@@ -9,6 +9,12 @@ describe('Main Module', () => {
     for (const fileName of files) {
       const file = new File([fs.readFileSync(fileName)], path.basename(fileName));
       const matches = await matchFile(file);
+
+      if (matches.length !== 1) {
+        console.error('Multiple parsers for', fileName);
+        console.error(matches.map(m => m.name));
+      }
+
       expect(matches.length).toBe(1);
     }
   });
